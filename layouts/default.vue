052a5fb7ab0569e45b41901363f6fa7773ef2269
Satindar <ibrahimasif711@outlook.com>
2023-06-19 15:17:35 +0530

    Added livechat
    Added livechat powered by tidio

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@
             <slot />
         </div>
     </div>
+    <script src="//code.tidio.co/hbmaiugetlvg7ncredz6anvzfkct12h5.js" async></script>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
